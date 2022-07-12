887c51271b011993376928d93794444b1c5170b1
FilipCichowski <58987436+FilipCichowski@users.noreply.github.com>
2022-07-12 10:02:46 +0200

    heroku deploy alternative

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+//Install express server
 const express = require("express");
 const path = require("path");
+
 const app = express();
-app.use(express.static(__dirname + "/dist/monte-cinema"));
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/monte-cinema/index.html"));
-});
+
+// Serve only the static files form the dist directory
+app.use(express.static("./dist/monte-cinema"));
+
+app.get("/*", (req, res) =>
+  res.sendFile("index.html", { root: "dist/monte-cinema/" })
+);
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
